7c1651e954d225ea4a801e60e4cbdabb050bafe2
jeanmichelly <jeanmichely@gmail.com>
2015-01-03 21:17:59 +0100

    Gestion security mot de passe

--- login.php
+++ login.php
@@ -2,6 +2,8 @@
 require_once('database/db.php');
 require_once('model/user.php');
 
+$salt = "ps2";
+
 $parameters = array(
 	':email' => null,
 	':password' => null
@@ -16,6 +18,9 @@ $json = array(
 );
 
 $config = require_once('database/config.php');
+
+$parameters[':password'] = sha1(md5($salt).md5($parameters[':password']));
+
 $db = new DB($config['dsn'], $config['username'], $config['password'], $config['options']);
 
 $user = $db->find('User', 'user', 'email = :email AND password = :password', $parameters);
